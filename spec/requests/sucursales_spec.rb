328b0838bf6bea4c10e6e163aaf7089ce554e2c4
Agustin Silva <silva.agustinn@gmail.com>
2022-12-01 14:52:39 -0300

    Agrega tests para SucursalesController con usuarios con rol de personal_bancario

--- spec/requests/sucursales_spec.rb
+++ spec/requests/sucursales_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 require 'support/session'
 
 RSpec.describe "Sucursales", type: :request do
+  let(:sucursal) { create(:sucursal) }
+
   context 'as a user with CLIENTE rol' do
     include_context :login_cliente_user
-
-    let(:sucursal) { create(:sucursal) }
 
     describe 'GET /index' do
       before { get(sucursales_url) }
@@ -60,6 +60,67 @@ RSpec.describe "Sucursales", type: :request do
 
       it 'should NOT destroy a sucursal' do
         expect(response).to redirect_to(edit_user_url(@cliente_user))
+      end
+    end
+
+  end
+
+  context 'as a user with PERSONAL_BANCARIO rol' do
+    include_context :login_personal_bancario_user
+
+    describe 'GET /index' do
+      before { get(sucursales_url) }
+
+      it 'should render index page' do
+        expect(response).to be_successful
+      end
+    end
+
+    describe 'GET /show' do
+      before { get(sucursal_url(sucursal)) }
+
+      it 'should render show page' do
+        expect(response).to be_successful
+      end
+    end
+
+    describe 'GET /new' do
+      before { get(new_sucursal_url) }
+
+      it 'should not render new page' do
+        expect(response).to redirect_to(edit_user_url(@personal_bancario_user))
+      end
+    end
+
+    describe 'GET /create' do
+      before { post(sucursales_url, params: { sucursal: attributes_for(:sucursal) }) }
+
+      it 'should not create a sucursal' do
+        expect(response).to redirect_to(edit_user_url(@personal_bancario_user))
+      end
+    end
+
+    describe 'GET /edit' do
+      before { get(edit_sucursal_url(sucursal)) }
+
+      it 'should not render edit page' do
+        expect(response).to redirect_to(edit_user_url(@personal_bancario_user))
+      end
+    end
+
+    describe 'GET /update' do
+      before { put(sucursal_url(sucursal), params: { sucursal: attributes_for(:sucursal) }) }
+
+      it 'should not update a sucursal' do
+        expect(response).to redirect_to(edit_user_url(@personal_bancario_user))
+      end
+    end
+
+    describe 'GET /destroy' do
+      before { delete(sucursal_url(sucursal)) }
+
+      it 'should not destroy a sucursal' do
+        expect(response).to redirect_to(edit_user_url(@personal_bancario_user))
       end
     end
 
--- spec/support/session.rb
+++ spec/support/session.rb
@@ -5,6 +5,14 @@ RSpec.shared_context :login_cliente_user, shared_context: :metadata do
   end
 end
 
+RSpec.shared_context :login_personal_bancario_user, shared_context: :metadata do
+  before do
+    @personal_bancario_user = FactoryBot.create(:user, :personal_bancario)
+    sign_in(@personal_bancario_user)
+  end
+end
+
 RSpec.configure do |rspec|
   rspec.include_context :login_cliente_user, include_shared: true
+  rspec.include_context :login_personal_bancario_user, include_shared: true
 end
