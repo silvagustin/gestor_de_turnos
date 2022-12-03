43cb3de2d44510c66890c4c5c968326255edbded
Agustin Silva <silva.agustinn@gmail.com>
2022-12-02 22:25:36 -0300

    Arregla un error en _navbar y mejora el archivo de seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,40 @@
 require 'logger'
 
-def crear_usuarios
-  users_attrs = [
-    # User 1
-    {
-      email: "silva.agustinn@gmail.com",
-      password: "123123123",
-      password_confirmation: "123123123",
-      rol: :administrador
-    }
-  ]
-
-  for user_attrs in users_attrs
-    User.create(user_attrs)
-  end
+def crear_clientes
+  20.times { FactoryBot.create(:user) }
 end
 
+def crear_personal_bancario
+  5.times { FactoryBot.create(:user, :personal_bancario) }
+end
+
+def crear_administradores
+  FactoryBot.create(:user, :administrador, email: 'administrador@email.com')
+end
+
+def crear_usuarios
+  crear_clientes
+  crear_personal_bancario
+  crear_administradores
+end
+
+def crear_sucursales
+  10.times { FactoryBot.create(:sucursal) }
+end
+
+###############################################################################
+# Main
+###############################################################################
 logger = Logger.new(STDOUT)
 
 logger.info("Eliminando todos los usuarios de la BD")
 User.destroy_all
 
 logger.info("Creando usuarios")
-crear_usuarios()
+crear_usuarios
+
+logger.info("Eliminando todas las sucursales de la BD")
+Sucursal.destroy_all
+
+logger.info("Creando sucursales")
+crear_sucursales
