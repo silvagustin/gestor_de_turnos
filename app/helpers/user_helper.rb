2f527b3d3ea03b684c14278630863c1718c801da
Agustin Silva <silva.agustinn@gmail.com>
2022-12-08 08:41:12 -0300

    Elimina 'Cliente' de roles seleccionables en el formulario de users

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,5 +1,9 @@
 module UserHelper
-  def opciones_sucursales
+  def roles_seleccionables
+    User.rols.reject { |rol| rol == 'cliente' }
+  end
+
+  def sucursales_seleccionables
     Sucursal.all.map { |s| [s.nombre, s.id] }
   end
 
